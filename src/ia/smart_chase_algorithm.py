c53152f2e6b4bc8676e6ecca53730f90b6470277
Juanse-Yarce <jlopezyarce@gmail.com>
2025-05-28 10:10:22 -0500

    cree la clase AlgoritmoPersecucionInteligente y agregue sus parametros

---
+++ src/ia/smart_chase_algorithm.py
@@ -0,0 +1,11 @@
+class AlgoritmoPersecucionInteligente:
+  def __init__(self, ancho_mapa, alto_mapa, cell_size=15):
+    self.ancho_mapa = ancho_mapa
+        self.alto_mapa = alto_mapa
+        self.cell_size = cell_size
+        self.cols = ancho_mapa // cell_size
+        self.rows = alto_mapa // cell_size
+        
+        self.historial_jugador = deque(maxlen=10) # Historial de posiciones del jugador para la predicción de movimiento.
+        self.poblacion_rutas = []
+    
