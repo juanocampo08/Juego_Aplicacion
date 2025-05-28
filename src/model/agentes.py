5db6e68e5696481c754b31fe949df361f14d57a3
JuanOcampo <juanjocampogomrz13@gmail.com>
2025-05-27 19:01:54 -0500

    se define el meotodo dibujar, este dibuja al agente con la ayuda de pygame con sus efectos futuristas

--- src/model/agentes.py
+++ src/model/agentes.py
@@ -53,6 +53,38 @@ class Agente:
         self.trail_positions.pop()
         self.trail_positions.insert(0, (self.x, self.y))
 
+    def dibujar(self, superficie):
+        if not self.esta_vivo:
+            return
+
+        center = (int(self.x), int(self.y))
+        tiempo_actual = pygame.time.get_ticks()
+        dano_reciente = (tiempo_actual - self.tiempo_dano) < 200
+
+        for i, pos in enumerate(self.trail_positions[1:]):
+            alpha = int(100 * (1 - i / len(self.trail_positions)))
+            trail_surface = pygame.Surface((self.radio, self.radio), pygame.SRCALPHA)
+            trail_color = (*self.color[:3], alpha)
+            pygame.draw.circle(trail_surface, trail_color, (self.radio // 2, self.radio // 2), max(1, self.radio - i * 2))
+            superficie.blit(trail_surface, (int(pos[0] - self.radio // 2), int(pos[1] - self.radio // 2)))
+
+        pulse_intensity = (math.sin(self.pulse_phase) + 1) / 2
+        glow_radius = int(self.radio + 10 + pulse_intensity * 8)
+
+        if dano_reciente and (tiempo_actual // 50) % 2:
+            glow_color = (255, 100, 100)
+        else:
+            glow_color = self.glow_color
+        VisualEffects.draw_glow_circle(superficie, glow_color, center, self.radio, glow_radius)
+
+
+        if self.agent_type == "player":
+            self._draw_player(superficie, center)
+        elif self.agent_type == "enemy":
+            self._draw_enemy(superficie, center)
+            self._draw_health_bar(superficie, center)
+        else:
+            self._draw_basic(superficie, center)
 
 
 
