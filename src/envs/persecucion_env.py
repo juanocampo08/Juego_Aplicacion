3ec079e1e4b7d9adabb451b1ba575a3dac9aca4f
Susana <sarazasusana2@gmail.com>
2025-05-27 22:43:39 -0500

    imports

--- src/envs/persecucion_env.py
+++ src/envs/persecucion_env.py
@@ -8,3 +8,9 @@ from gymnasium import spaces
 
 import sys
 
+from utils.mapa_utils import GeneradorDeMapas
+from ia.smart_chase_algorithm import AlgoritmoPersecucionInteligente, calcular_accion_inteligente
+from model.agentes import Jugador, Enemigo
+from model.entorno import ObstaculoFuturista, PowerUpSalud
+from utils.visual_effects import VisualEffects
+from utils.pantallas import pantalla_bienvenida, pantalla_game_over
