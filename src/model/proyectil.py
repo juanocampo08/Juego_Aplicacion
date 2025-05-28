1565359379dc77be05d2cec7ea50561c19675985
JuanOcampo <juanjocampogomrz13@gmail.com>
2025-05-27 21:30:00 -0500

    actualiza la posicion del proyectil, sus efectos y verifica colisiones.

--- src/model/proyectil.py
+++ src/model/proyectil.py
@@ -29,3 +29,24 @@ class Proyectil:
         self.trail_positions = [(x, y)] * 3
         self.pulse_phase = 0
 
+    def update(self, ancho_pantalla, alto_pantalla, obstaculos = []):
+        if not self.activo:
+            return
+
+        self.x += self.vel_x
+        self.y += self.vel_y
+
+        proyectil_rect = pygame.Rect(int(self.x - self.radio), int(self.y - self.radio), self.radio * 2, self.radio * 2)
+        for obstaculo in obstaculos:
+            if proyectil_rect.colliderect(obstaculo.rect):
+                self.activo = False
+                return
+
+        self.pulse_phase += 0.3
+        self.trail_positions.pop()
+        self.trail_positions.insert(0, (self.x, self.y))
+
+        if (self.x < 0 or self.x > ancho_pantalla or
+                self.y < 0 or self.y > alto_pantalla):
+            self.activo = False
+
